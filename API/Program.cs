19f6b84e449de2b376d7892918c6a67bc3c82f2a
EduardoGooj <mortalkiller666@gmail.com>
2024-09-05 07:41:54 -0600

    parcial01_Add user list to html

--- API/Program.cs
+++ API/Program.cs
@@ -8,11 +8,14 @@ builder.Services.AddDbContext<DataContext>(opt =>
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 }
 );
+builder.Services.AddCors();
+
 // Add services to the container.
 
 builder.Services.AddControllers();
-
 var app = builder.Build();
+// configure http request pipeline 
+app.UseCors(c => c.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200","https://localhost:4200"));
 
 app.MapControllers();
 
--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { CommonModule } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
 import { Component, inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
@@ -16,7 +17,7 @@ export class AppComponent implements OnInit {
   users: any;
 
   ngOnInit(): void {
-    this.http.get("https://localhost:5001/api/v1/users").subscribe({
+    this.http.get("http://localhost:5000/api/v1/User").subscribe({
       next: (response) => {this.users = response},
       error: (error) => {console.log(error)},
       complete: () => {console.log("Request completed!")}
