fc78469f459fc34f117094735cb962692ad1e0b8
EduardoGooj <mortalkiller666@gmail.com>
2024-09-17 07:18:23 -0600

    parcial01_addpassword to appuser

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -6,4 +6,8 @@ public class AppUser
 {
     public int Id {get;set;}= 0;
     public required string UserName {get;set;} 
+
+    public required byte[] passwordHash {get;set;}
+
+    public required byte[] passwordSalt {get;set;}
 }
