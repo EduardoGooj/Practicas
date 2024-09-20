7286015bd0e7bdc30ea1b1850c5fe421a0a17d08
EduardoGooj <mortalkiller666@gmail.com>
2024-09-19 22:18:04 -0600

    Parcial01_ Add token Service
    se hizo squash por modificación en token service

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -3,10 +3,14 @@ using System.Text;
 using API.Data;
 using API.DTOs;
 using API.Entities;
+using API.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 namespace API.Controllers;
-public class AccountController(DataContext context): BaseApiController{
+public class AccountController(
+    DataContext context,
+    ITokenService tokenService
+): BaseApiController{
     [HttpPost("register")]
     public async Task <ActionResult<AppUser>> RegisterAsync(RegisterRequest request){
         if (await UserExistsAsync(request.Username)) return BadRequest("Username already in use");
@@ -19,6 +23,31 @@ public class AccountController(DataContext context): BaseApiController{
         context.Users.Add(user);
         await context.SaveChangesAsync();
         return user;
+    }
+    [HttpPost("login")]
+    public async Task<ActionResult<UserRespose>> LoginAsync(LoginRequest request)
+    {
+        var user = await context.Users.FirstOrDefaultAsync(x=>
+        x.UserName.ToLower() == request.UserName.ToLower());
+
+        if (user == null){
+            return Unauthorized("invalid username o password");
+        }
+
+        using var hmac =new  HMACSHA512(user.passwordSalt);
+        var computeHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(request.Password));
+
+        for (int i=0; i< computeHash.Length ;i++){
+            if(computeHash[i] != user.passwordHash[i]){
+                return Unauthorized("invalid username or password");
+            }
+        
+
+        }
+        return new UserRespose{
+            UserName=user.UserName,
+            Token=tokenService.CreateToken(user)
+        };
     }
     private async Task<bool>UserExistsAsync(string username){
 
